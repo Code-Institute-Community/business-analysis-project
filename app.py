daefbfba6ef631e8bb16cc760478c4fd83a6ae2d
alissatroiano <alissatroianonyc@gmail.com>
2022-03-07 22:11:49 -0500

    Setup user dir with routes and Flask-User settings

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ import os
 import re
 from flask import (Flask, render_template, redirect, session, request, url_for)
 from flask_pymongo import PyMongo
+# Routes
+from user import routes
+# Import Emv
 if os.path.exists("env.py"):
     import env
 app = Flask(__name__)
-
+# Import Env Variables
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-
+# Import Mongo
 mongo = PyMongo(app)
 
-# Flask-User settings
+# Customize User settings
 USER_ENABLE_EMAIL = True      # Enable email authentication
 USER_ENABLE_USERNAME = True    # Enable username authentication
 USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
-
-# Routes
-from user import routes
+USER_ENABLE_REGISTER = True    # Enable register
 
 @app.route("/")
 @app.route("/get_index")
--- user/routes.py
+++ user/routes.py
@@ -8,19 +8,3 @@ from user.models import User
 @app.route("/user/register", methods=["POST"]) #use as GET to test in the browser
 def register():
     return User().register()
-
-
-""" A route to login users by email and validate credentials """
-app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        existing_user = mongo.db.users.find_one(
-            # Change to "username" to login with username
-            {"email": request.form.get("email")})
-        
-        if existing_user:
-            if existing_user["password"] == request.form.get("password"):
-                session["user"] = request.form.get("username").lower()
-                return redirect(url_for("dashboard", username=session["user"]))
-        
-        return redirect(url_for("login"))
