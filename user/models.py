b26ab826944a8f80050f8535277c3a9a587bc7ab
alissatroiano <alissatroianonyc@gmail.com>
2022-03-07 22:30:28 -0500

    Add password encryption to user model

--- user/models.py
+++ user/models.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify, request
+from passlib.hash import pbkdf2_sha256
 import uuid
 
 class User:
     
     def register(self):
+        print(request.form)
         
+        # Create the user object
         user = {
 			"_id": uuid.uuid4().hex,
 			"username": request.form.get("username"),
    			"email": request.form.get("email"),
       		"password": request.form.get("password")
 		}
+        
+        # Encrypt the password
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
         
         return jsonify(user), 200
