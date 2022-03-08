671e349d944a796404e88c4464cf0b2a34922367
alissatroiano <alissatroianonyc@gmail.com>
2022-03-07 22:17:15 -0500

    Add uuid to user auth models

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import os
 import re
-from flask import (Flask, render_template, redirect, session, request, url_for)
+from flask import (Flask, render_template)
 from flask_pymongo import PyMongo
 # Routes
 from user import routes
-# Import Emv
+
+# Import Env
 if os.path.exists("env.py"):
     import env
 app = Flask(__name__)
+
 # Import Env Variables
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
 # Import Mongo
 mongo = PyMongo(app)
 
--- user/models.py
+++ user/models.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
-
+import uuid
 
 class User:
     
     def register(self):
         
         user = {
-			"_id": "",
+			"_id": uuid.uuid4().hex,
 			"username": request.form.get("username"),
    			"email": request.form.get("email"),
       		"password": request.form.get("password")
