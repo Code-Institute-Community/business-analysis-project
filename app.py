07c582e34fa1a58cc879ca0171efcfb2869f9309
John Doyle <johndoyle@Johns-MacBook-Pro.local>
2022-03-09 15:07:53 +0000

    add add_business logic and template

--- app.py
+++ app.py
@@ -12,7 +12,39 @@ def hello():
     return "In the words of Théoden....<br>'So... it beings...'"
 
 
+@app.route("/add_business", methods=["GET", "POST"])
+def add_business():
+    if request.method == "POST":
+        organisation_name = request.form.get("organisation_name")
+        latitude = request.form.get("latitude")
+        longitude = request.form.get("longitude")
+        nace_1 = request.form.get("nace_1")
+        nace_1_label = request.form.get("nace_1_label")
+        nace_2 = request.form.get("nace_2")
+        nace_2_label = request.form.get("nace_2_label")
+        nace_3 = request.form.get("nace_3")
+        nace_3_label = request.form.get("nace_3_label")
+        web_address = request.form.get("web_address")
+        business = {
+            "organisation_name": organisation_name,
+            "latitude": latitude,
+            "longitude": longitude,
+            "nace_1": nace_1,
+            "nace_1_label": nace_1_label,
+            "nace_2": nace_2,
+            "nace_2_label": nace_2_label,
+            "nace_3": nace_3,
+            "nace_3_label": nace_3_label,
+            "web_address": web_address,
+        }
+        mongo.db.business_list.insert_one(business)
+        # flash(" - Business Successfully Added - ")
+        return redirect(url_for("home"))
+
+    return render_template("add-business.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)  
+            debug=True)
