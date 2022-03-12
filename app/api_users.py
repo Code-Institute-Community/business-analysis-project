c0f75df78614080f722c135fd29b6f6f0c9d7e13
Tom-Nagy <tomnagy@hotmail.fr>
2022-03-12 22:35:32 +0000

    Add sort by categories template logic

--- app/api_users.py
+++ app/api_users.py
@@ -12,9 +12,22 @@ api_users = Blueprint("api_users", __name__)
 # collection
 data_set_coll = mongo.db.SampleData
 
-@api_users.route("/all_companies", methods=["GET", "POST"])
+@api_users.route("/all_companies", methods=["GET"])
 def all_companies():
-    
+    # Get the data_set from db
     data_set = list(data_set_coll.find())
-    
-    return render_template("all_companies.html", data_set=data_set)
+
+    # catgories sample set that would be populated/created dynamically 
+    categories = [
+        {"category": "information",
+         "sub_categories": ["technologies", "product"]
+        },
+        {"category": "communication",
+         "sub_categories": ["tech", "prod"]
+        },
+        {"category": "programming",
+         "sub_categories": ["frontend", "backend"]
+        }]
+
+    return render_template("all_companies.html",
+                           data_set=data_set, categories=categories)
