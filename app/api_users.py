651f3499b13d06b47253f41b5e17bae8bd5599cf
Tom-Nagy <tomnagy@hotmail.fr>
2022-03-12 20:45:47 +0000

    Add list of companies

--- app/api_users.py
+++ app/api_users.py
@@ -1,16 +1,20 @@
 """
 Views related to api_user.
 """
+from app import mongo
 from flask import (
     Flask, flash, render_template, redirect,
     request, session, url_for, Blueprint, current_app)
 
-
+# Blueprint
 api_users = Blueprint("api_users", __name__)
+
+# collection
+data_set_coll = mongo.db.SampleData
 
 @api_users.route("/all_companies", methods=["GET", "POST"])
 def all_companies():
     
-
+    data_set = list(data_set_coll.find())
     
-    return render_template("all_companies.html")
+    return render_template("all_companies.html", data_set=data_set)
