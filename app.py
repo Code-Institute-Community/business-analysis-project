245f74f292fbb97739422e20d01851320ac99fc7
alissatroiano <alissatroianonyc@gmail.com>
2022-03-13 17:19:53 -0400

    Move forms to file to cleanup code and resolve conflicts

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from curses.ascii import EM
 import os
+import json
 from flask import (
-                   Flask, flash, redirect, render_template, 
-                   url_for, session, request)
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,13 +11,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import Email, InputRequired, Length
+# Import authentication forms
+from forms import Login, Register
 
 # Import Flask-Bootstrap
 from flask_bootstrap import Bootstrap
-# Import certifi
+# Import certifi to validate ssl certificates
 import certifi
 
-if os.path.exists("env.py"):
+if os.path.exists('env.py'):
     import env
 
 app = Flask(__name__)
@@ -30,20 +33,12 @@ mongo = PyMongo(app, tlsCAFile=certifi.where())
 collection_name = mongo.db.collection_name
 users = mongo.db.users
 
-class Register(FlaskForm):
-    username = StringField('Enter Your Name', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Enter Your Password', validators=[InputRequired(), Length(min=4, max=15)])
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(min=6, max=50)])
-class Login(FlaskForm):
-    username = StringField('Enter Your Name', validators=[InputRequired()])
-    password = PasswordField('Enter Your Password', validators=[InputRequired()])
-    remember = BooleanField('Remember Me')
-
 @app.route("/")
 @app.route("/index")
 def index():
     # users = mongo.db.users.find()
     return render_template('index.html')
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -99,13 +94,31 @@ def login():
 
 @app.route("/logout")
 def logout():
+    user = session.get("user")
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
 
 
+# Routes related to crud of organisations
+@app.route('/organisations', methods=['GET', 'POST'])
+def get_organisations():
+    '''
+    Display a list of all organisations in table format for admin user
+    '''
+    organisations = mongo.db.organisations.find()
+    return render_template('organisations_list.html',
+                           organisations=organisations)
+
+
+# TODO: define user role permission for admin and user. When user submits
+# the form, will admin have to check the submission and approve before
+# it is added to the database?
 @app.route("/add_business", methods=["GET", "POST"])
-def add_business():
+def create_organisation():
+    '''
+    Add an organisation for normal user and admin user
+    '''
     if request.method == "POST":
         organisation_name = request.form.get("organisation_name")
         latitude = request.form.get("latitude")
@@ -129,11 +142,58 @@ def add_business():
             "nace_3_label": nace_3_label,
             "web_address": web_address,
         }
-        mongo.db.business_list.insert_one(business)
+        mongo.db.organisations.insert_one(business)
         # flash(" - Business Successfully Added - ")
-        return redirect(url_for("home"))
+        return redirect(url_for("get_organisations"))
 
-    return render_template("add-business.html")
+    return render_template("create_organisation.html")
+
+
+@app.route('/organisations/<organisation_id>/edit', methods=['GET', 'POST'])
+def edit_organisation(organisation_id):
+    '''
+    Edit an organisation for admin user
+    '''
+    if request.method == 'POST':
+        organisation_name = request.form.get("organisation_name")
+        latitude = request.form.get("latitude")
+        longitude = request.form.get("longitude")
+        nace_1 = request.form.get("nace_1")
+        nace_1_label = request.form.get("nace_1_label")
+        nace_2 = request.form.get("nace_2")
+        nace_2_label = request.form.get("nace_2_label")
+        nace_3 = request.form.get("nace_3")
+        nace_3_label = request.form.get("nace_3_label")
+        web_address = request.form.get("web_address")
+        edit_org = {
+            "organisation_name": organisation_name,
+            "latitude": latitude,
+            "longitude": longitude,
+            "nace_1": nace_1,
+            "nace_1_label": nace_1_label,
+            "nace_2": nace_2,
+            "nace_2_label": nace_2_label,
+            "nace_3": nace_3,
+            "nace_3_label": nace_3_label,
+            "web_address": web_address,
+        }
+        mongo.db.organisations.update_one({'_id': ObjectId(organisation_id)},
+                                          {'$set': edit_org})
+        return redirect(url_for('get_organisations'))
+
+    organisation = mongo.db.organisations.find_one(
+        {'_id': ObjectId(organisation_id)})
+    return render_template('edit_organisation.html',
+                           organisation=organisation)
+
+
+@app.route('/organisations/<organisation_id>/delete', methods=['GET', 'POST'])
+def delete_organisation(organisation_id):
+    '''
+    Delete an organisation for admin user
+    '''
+    mongo.db.organisations.delete_one({'_id': ObjectId(organisation_id)})
+    return redirect(url_for('get_organisations'))
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -2,13 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import Email, InputRequired, Length
 
-app = Flask(__name__)
-Bootstrap(app)
-
 class Register(FlaskForm):
     username = StringField('Enter Your Name', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('Enter Your Password', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(min=6, max=50)])
+
 class Login(FlaskForm):
     username = StringField('Enter Your Name', validators=[InputRequired()])
     password = PasswordField('Enter Your Password', validators=[InputRequired()])
