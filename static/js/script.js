6faf43218c6d2fa4735cb6ac2dcab17a9f7b4a15
alissatroiano <alissatroianonyc@gmail.com>
2022-03-15 23:03:40 -0400

    Remove mongo details from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,9 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://alissa:Apple10@cluster0.fmzii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = os.getenv('MONGODB_URI');
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("user");
   // perform actions on the collection object
   client.close();
 });
+// end::script[]
