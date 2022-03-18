b45dc1b0118204dda677bd69407edae19537b7e4
Evelyn Foy <evelyn.foy@hotmail.com>
2022-03-18 20:21:31 +0000

    Refactor accessing of companies list from python

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -17,19 +17,16 @@ var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
   accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 }).addTo(map);
 
-// Get companies details from hidden div on index.html with id = data
-const companiesArrayJson = document.getElementById("data").innerHTML.replaceAll("'", '"');
-companies = JSON.parse(companiesArrayJson);
 
-// Add marker to map for each company
+/**
+ * Adds marker to map for each company.
+ * Companies array is populated in an inline script in base.html. 
+ * When clicked Company name and webaddress are displayed in a pop-up. 
+ * 
+ *  */ 
 var markers = [];
-for (let i=0; i< companies.length; i++) {
-  console.log(companies[i].organisation_name);
+for (let i = 0; i < companies.length; i++) {
   markers[i] = L.marker([companies[i].latitude, companies[i].longitude]).addTo(map)
-  .bindPopup(companies[i].organisation_name  + '<br>' + companies[i].web_address)
+    .bindPopup(companies[i].organisation_name + '<br>' + companies[i].web_address)
     .openPopup();
 }
-
-
-
-
