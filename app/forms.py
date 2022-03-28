75c9353eaf420de7a2f69be9831f230856adf443
Developer <dev@Alissas-MBP.fios-router.home>
2022-03-27 20:57:47 -0400

    Update form styles with color palette

--- app/forms.py
+++ app/forms.py
@@ -10,19 +10,18 @@ csrf = CSRFProtect()
 class RegisterForm(FlaskForm):
     first_name = StringField()
     last_name = StringField()
-    username = StringField('Enter Your Name', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Enter Your Password', validators=[InputRequired(), Length(min=4, max=15)])
+    username = StringField('Choose a Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Choose a Password', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(min=6, max=50)])
     submit = SubmitField('Register')
 
 
 '''A form to login a user. '''
 class LoginForm(FlaskForm):
-    username = StringField('Enter Your Name', validators=[InputRequired()])
-    password = PasswordField('Enter Your Password', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
 
 ''' A form to reset a user's password. '''
 class ResetPasswordForm(FlaskForm):
