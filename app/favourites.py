d9821352d577d7be91fbb5197ea721f23f646937
Stefan Dworschak <stef.dworschak@gmail.com>
2022-03-31 15:19:47 +0100

    Use current_user instead of custom User function

--- app/favourites.py
+++ app/favourites.py
@@ -6,8 +6,7 @@ from flask import (
     redirect, request, session, url_for, jsonify,
     Blueprint)
 
-from flask_login import login_required
-from bson.objectid import ObjectId
+from flask_login import current_user, login_required
 
 from app import mongo
 
@@ -25,37 +24,33 @@ def view_favourites():
     - retrieve user in session
     - find organisations in list of user's favourites
     """
-    user = User.find_one_user(session["_user_id"].lower())
-    favourites = list(mongo.db.organisations.find({"_id": {
-                                                    "$in": user["favourites"]}
-                                                   }))
     return render_template('favourites/my_favourites.html',
-                           favourites=favourites)
+                           favourites=current_user.get_favourites())
 
 
 @favourites.route("/add_to_favourites", methods=["GET", "POST"])
 @login_required
 def add_to_favourites():
     """
-    Function to add a company to a user's favourite, requiring user to be
+    Function to add a organisation to a user's favourite, requiring user to be
     logged-in
     - Post request received from script.js / organisation.html
-    - Retrieve the company ID from the data
-    - Check if company has been added to favourites
-    - Add company id to favourites field in user collection in MongoDB
+    - Retrieve the organisation ID from the data
+    - Check if organisation has been added to favourites
+    - Add organisation id to favourites field in user collection in MongoDB
     - Return success message
     """
-    # Retrieve company id from post request
+    # Retrieve organisation id from post request
     resp = request.form.to_dict(flat=False)
-    company_id = resp["companyId"][0]
+    organisation_id = resp["organisationId"][0]
     # Retrieve user in session
     user = User.find_one_user(session["_user_id"].lower())
-    if "favourites" in user and company_id in user["favourites"]:
-        flash("Company already added to favourites")
+    if "favourites" in user and organisation_id in user["favourites"]:
+        flash("Organisation already added to favourites")
         return redirect(url_for('organisations.get_organisations'))
     else:
-        # Add company ID to user's favourite
-        User.append_favourite(user['_id'], company_id)
+        # Add organisation ID to user's favourite
+        User.append_favourite(user['_id'], organisation_id)
         message = "success"
 
     return jsonify(message)
@@ -65,20 +60,19 @@ def add_to_favourites():
 @login_required
 def remove_from_favourites():
     """
-    Function to remove a company from a user's favourite
+    Function to remove a organisation from a user's favourite
     - requires user to be logged-in
     - Post request received from script.js / my_favourites.html
-    - Retrieve the company ID from the data and removes it from array
+    - Retrieve the organisation ID from the data and removes it from array
       "favourites"
     - Return success message
     """
-    # Retrieve company id from post request
+    # Retrieve organisation id from post request
     resp = request.form.to_dict(flat=False)
-    company_id = resp["companyId"][0]
-    # Retrieve user in session
-    user = User.find_one_user(session["_user_id"].lower())
-    if "favourites" in user:
-        User.remove_from_favourites(user['_id'], company_id)
+    organisation_id = resp["organisationId"][0]
+
+    if organisation_id in current_user.get_favourites():
+        current_user.remove_from_favourites(organisation_id)
         message = "success"
         return jsonify(message)
     else:
