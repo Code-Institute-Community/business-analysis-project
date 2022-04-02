37731837c9e2fa18c15aec8672bb91fce9ba3b84
Arthur O'Leary <arthur.oleary24@mail.dcu.ie>
2022-04-02 17:03:17 +0100

    Refactor webscraper function

--- app/functions/company_web_scraper.py
+++ app/functions/company_web_scraper.py
@@ -2,41 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-
+def scraper(url_to_scrape):
 # function to extract html document from given url
-def getHTMLdocument(url):
-
-    # request for HTML document of given url
-    response = requests.get(url)
-
-    # response will be provided in JSON format
-    return response.text
+    def getHTMLdocument(url):
+        
+        # request for HTML document of given url
+        response = requests.get(url)
+        
+        # response will be provided in JSON format
+        return response.text
+        
+    # url_to_scrape = "https://codeinstitute.net/ie/"
     
-# url_to_scrape = "https://codeinstitute.net/ie/"
-  
-# # create document
-# html_document = getHTMLdocument(url_to_scrape)
+    # create document
+    html_document = getHTMLdocument(url_to_scrape)
+    
+    # create soap object
+    soup = BeautifulSoup(html_document, 'html.parser')
+    
+    
+    # find all the anchor tags with "href" 
+    # attribute starting with "https:// and the about keyword"
+    # remove | about if you want to return all links on landing page
+    links = []
+    for link in soup.find_all('a', 
+                            attrs={'href': re.compile(r'^http://|about')}):
+        links.append(link.get('href'))
+    # display the actual urls
+    print(links[0])
 
-# # create soap object
-# soup = BeautifulSoup(html_document, 'html.parser')
 
-# # find all the anchor tags with "href" 
-# # attribute starting with "https:// and the about keyword"
-# # remove | about if you want to return all links on landing page
-# links = []
-# for link in soup.find_all('a', 
-#                           attrs={'href': re.compile(r'^http://|about')}):
-#     links.append(link.get('href'))
-# # display the actual urls
-# print(links[0])
+    URL = links[0]
+    page = requests.get(URL)
 
+    soup = BeautifulSoup(page.content, "html.parser")
+    # results = soup.find(id="ResultsContainer")
 
-# URL = links[0]
-# page = requests.get(URL)
-
-# soup = BeautifulSoup(page.content, "html.parser")
-# # results = soup.find(id="ResultsContainer")
-
-# for words in soup.find_all('p'):
-#     find_all_words = words.get_text()
-#     print(find_all_words)
+    for words in soup.find_all('p'):
+        find_all_words = words.get_text()
+        print(find_all_words)
