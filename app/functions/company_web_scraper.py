eb4ad2dd12ce91925ebe4e519da60d23f2ed57d7
Arthur O'Leary <arthur.oleary24@mail.dcu.ie>
2022-04-06 17:56:06 +0100

    Modified scraper function scope, added docstring.

--- app/functions/company_web_scraper.py
+++ app/functions/company_web_scraper.py
@@ -3,22 +3,20 @@ from bs4 import BeautifulSoup
 import re
 
 def scraper(url_to_scrape):
-# function to extract html document from given url
-    def getHTMLdocument(url):
-        
-        # request for HTML document of given url
-        response = requests.get(url)
-        
-        # response will be provided in JSON format
-        return response.text
-    
+    '''
+    Function to extract html document from given url
+    Pass string URL as argument to function
+    '''
+    # request for HTML document of given url
+    response = requests.get(url_to_scrape)
+
+    # response will be provided in JSON format
     # create document
-    html_document = getHTMLdocument(url_to_scrape)
-    
-    # create soap object
+    html_document = response.text
+
+    # create soup object
     soup = BeautifulSoup(html_document, 'html.parser')
-    
-    
+
     # find all the anchor tags with "href" 
     # attribute starting with "https:// and the about keyword"
     # remove | about if you want to return all links on landing page
@@ -29,12 +27,10 @@ def scraper(url_to_scrape):
     # display the actual urls
     print(links[0])
 
-
     URL = links[0]
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
-    # results = soup.find(id="ResultsContainer")
 
     for words in soup.find_all('p'):
         find_all_words = words.get_text()
