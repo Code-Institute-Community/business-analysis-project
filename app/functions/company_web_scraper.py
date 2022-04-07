7d11c5265624ad060cce7439f82283e6dab5e9a6
Arthur O'Leary <arthur.oleary24@mail.dcu.ie>
2022-04-07 12:33:31 +0100

    Moved getHTMLdocument function to its own line.

--- app/functions/company_web_scraper.py
+++ app/functions/company_web_scraper.py
@@ -2,6 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
+def getHTMLdocument(url):
+    # request for HTML document of given url
+    response = requests.get(url)
+    # response will be provided in JSON format
+    return response.text
+
 def scraper(url_to_scrape):
     '''
     Function to extract html document from given url
