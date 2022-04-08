c0900c624267f595e61feee3bb673b9177a41d2b
Evelyn Foy <evelyn.foy@hotmail.com>
2022-04-08 19:59:39 +0100

    Add categories to demo data and change seed.py to remove old versions

--- seed.py
+++ seed.py
@@ -11,6 +11,7 @@ def seed():
     mongo_client = MongoClient(os.environ.get('MONGO_URI'))
     mongo_db = mongo_client[os.environ.get('MONGO_DBNAME')]
     orgs = json.load(open('data/demo_data.json'))
+    mongo_db.organisations.delete_many({})
     mongo_db.organisations.insert_many(orgs)
 
 if __name__ == '__main__':
