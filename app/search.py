32bf80e59754b0d59727f1b0339c890f2a2cf8ea
Alissa Troiano <alissatroianonyc@gmail.com>
2022-04-12 07:14:27 -0400

    Setup search blueprint and routes

--- app/search.py
+++ app/search.py
@@ -12,18 +12,23 @@ from app.models.user import User
 # Blueprint
 search = Blueprint("search", __name__, template_folder='templates')
 
-@search.route('/search', methods=['POST'])
+@search.route('/search', methods=['GET', 'POST'])
 @login_required
 def get_search():
   form = SearchForm()
-  if not form.validate_on_submit():
-    return redirect(url_for('home/home.html'))
-  return redirect((url_for('search.search_results', query=form.search.data)))
+  data = request.form.get("query")
+  if request.method == 'POST' and form.validate_on_submit():
+      return redirect((url_for('search.search_results', query=form.search.data)))
+  return redirect('home/home.html', form=form)
 
 
 @search.route('/search_results/<query>')
 @login_required
 def search_results(query):
-  organisations = list(mongo.db.organisations.find())
-  results = mongo.db.organisation.query.find_one(query)
-  return render_template('search/search_results.html', query=query, results=results)
+  organisations = mongo.db.organisations.find()
+  query = query.lower()
+  results = []
+  for organisation in organisations:
+    if query in organisation['organisation_name'].lower():
+      results.append(organisation)
+  return render_template('search/search_results.html', results=results)
