f84458e78a10c2413762f30e9d8ca944dc1ebe20
AwsSG <aws.sabah.gheni@gmail.com>
2022-11-23 15:24:43 +0000

    fixed list appaneing on page reload, removed duplicate keywords

--- app/keywords.py
+++ app/keywords.py
@@ -16,28 +16,30 @@ data = json.load(open('data/full_data_with_keywords.json'))
 
 all_keywords = [] # all the keywords with their ranking
 
+key_list = [] # clean keywords-only list
+
+unq_keys = [] # list of unique keys (no duplicate keywords)
 
 @keywords.route("/")
 def view_keywords():
     # Display keywords page
     # website_text --- keywords
     # all_keywords = list(mongo.db.organisations.find())
-    key_list = [] # clean keywords-only list
-    total = 0
-    for item in data:
-        if 'keywords' in item: # check if obj has keywords key
-            if item['keywords']: # check if keywords is not empty
-                all_keywords.append(item['keywords'])
-    
-    for item in all_keywords: # clean up keys list
-        if type(item) == list:
-            for value in item:
-                key = list(value.keys())
-                key_list.append(key[0])
-    total = len(key_list)
-    print(total)
+    if len(all_keywords) == 0:
+        for item in data:
+            if 'keywords' in item: # check if obj has keywords key
+                if item['keywords']: # check if keywords is not empty
+                    all_keywords.append(item['keywords'])
+    print(len(all_keywords))
+    if len(key_list) == 0:
+        for item in all_keywords: # clean up keys list
+            if type(item) == list:
+                for value in item:
+                    key = list(value.keys())
+                    key_list.append(key[0])
+    unq_keys = list(dict.fromkeys(key_list)) # remove duplicate keyswords
+    print(len(key_list))
     # counts = dict(Counter(key_list))
     # duplicates = {key:value for key, value in counts.items() if value > 1} # find the repetition of each keyword
-    # unq_keys = mylist = list(dict.fromkeys(key_list)) # remove duplicate keyswords
     
-    return render_template('keywords/keywords.html', key_list=key_list)
+    return render_template('keywords/keywords.html', key_list=key_list, unq_keys=unq_keys)
